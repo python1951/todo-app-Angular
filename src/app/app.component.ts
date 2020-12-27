41c42822b48f78ba976ba510b4297512c5b727fe
python1951 <qamarha28@gmail.com>
2020-12-27 14:53:05 +0500

    todo-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo-App';
+  todos = [
+    {
+      label:"Eating Breakfast",
+    },
+    {
+      label:"Giving Lunch",
+    },
+    {
+      label:"Going to Office",
+    }
+  ];
+  addTodo(newtodolabel: any){
+   var  newtodo ={
+     label : newtodolabel,
+   }
+   this.todos.push(newtodo);
+  }
+  deleteTodo(todo: any){
+    this.todos = this.todos.filter( p=> p.label !== todo.label)
+
+
+  }
 }
